bcaac7b5522f3d7d09f54cefa26a01491f23e103
JeongBin Park <obb8923@naver.com>
2024-11-19 19:33:20 +0900

    feat/nav design

--- src/nav/tabNav/App/index.tsx
+++ src/nav/tabNav/App/index.tsx
@@ -4,18 +4,30 @@ import HomeStackNav from "../../../nav/stackNav/Home";
 import LetterStackNav from "../../../nav/stackNav/Letter";
 import SystemStackNav from "../../../nav/stackNav/System";
 //svgs
+import HomeIcon from "../../../../assets/svgs/Home.svg";
+import LetterIcon from "../../../../assets/svgs/Letter.svg";
+import SystemIcon from "../../../../assets/svgs/System.svg";
+//
 import { TabNavOptions } from "../../../libs/constants/TabNavOptions";
 const Tab = createBottomTabNavigator();
 
 const AppTabNav = () => {
-    return (
-        <Tab.Navigator 
-        screenOptions={TabNavOptions as BottomTabNavigationOptions}>
-          <Tab.Screen name="HomeStackNav" component={HomeStackNav} />
-          <Tab.Screen name="LetterStackNav" component={LetterStackNav} />
-          <Tab.Screen name="SystemStackNav" component={SystemStackNav} />
-        </Tab.Navigator>
-    )
+  return (
+      <Tab.Navigator 
+      screenOptions={TabNavOptions as BottomTabNavigationOptions}>
+        <Tab.Screen name="HomeStackNav" component={HomeStackNav} options={{
+          tabBarLabel:'Home',
+          tabBarIcon: ({ focused }) =>  focused ? <HomeIcon style={{color:'#fafafa'}}/> : <HomeIcon style={{color:'#585A6C'}}/>,
+
+        }} />
+        <Tab.Screen name="LetterStackNav" component={LetterStackNav} options={{tabBarLabel:'Letter',
+            tabBarIcon: ({ focused }) =>  focused ? <LetterIcon style={{color:'#fafafa'}}/> : <LetterIcon style={{color:'#585A6C'}}/>,
+        }} />
+        <Tab.Screen name="SystemStackNav" component={SystemStackNav} options={{tabBarLabel:'System',
+            tabBarIcon: ({ focused }) =>  focused ? <SystemIcon style={{color:'#fafafa'}}/> : <SystemIcon style={{color:'#585A6C'}}/>,
+        }} />
+      </Tab.Navigator>
+  )
 }
 
 export default AppTabNav;
