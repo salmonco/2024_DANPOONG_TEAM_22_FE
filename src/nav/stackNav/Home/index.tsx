e7f1c507b6107198184333eb23f326b84f8ffc38
JeongBin Park <obb8923@naver.com>
2024-11-19 20:18:58 +0900

    feat/tab hide
    탭이 homescreen에서만 보이도록 수정했습니다.
    스택 네비게이터들어오고나갈때
    어떤스크린인지  확인해서
    숨깁니다.

--- src/nav/stackNav/Home/index.tsx
+++ src/nav/stackNav/Home/index.tsx
@@ -1,19 +1,31 @@
 import HomeScreen from "../../../screens/Home";
-import RCDFeedBackScreen from '../../../screens/RCDFeedBack';
-import RCDListScreen from '../../../screens/RCDList';
-import RCDNoticeScreen from '../../../screens/RCDNotice';   
-import RCDRecordScreen from '../../../screens/RCDRecord';
-import RCDTextScreen from '../../../screens/RCDText';
-import RCDSelectTextScreen from '../../../screens/RCDSelectText';
+import RCDFeedBackScreen from "../../../screens/RCDFeedBack";
+import RCDListScreen from "../../../screens/RCDList";
+import RCDNoticeScreen from "../../../screens/RCDNotice";
+import RCDRecordScreen from "../../../screens/RCDRecord";
+import RCDTextScreen from "../../../screens/RCDText";
+import RCDSelectTextScreen from "../../../screens/RCDSelectText";
 //navigation
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { HomeStackParamList } from '../../../types/HomeStackParamList'
-
+import {getFocusedRouteNameFromRoute, NavigationProp, useNavigation, useRoute} from '@react-navigation/native';
+import { useLayoutEffect } from "react";
+import { TabNavOptions } from '../../../libs/constants/TabNavOptions'
 
 const Stack = createNativeStackNavigator<HomeStackParamList>();
 
 const HomeStackNav = () => {
-  
+  const navigation = useNavigation<NavigationProp<HomeStackParamList>>()
+  const route = useRoute()
+  useLayoutEffect(() => {
+    const routeName = getFocusedRouteNameFromRoute(route);
+    // console.log(route,routeName)
+    if (routeName === undefined || routeName === 'Main') { //Main이외의 화면에 대해 tabBar none을 설정한다.
+      navigation.setOptions(TabNavOptions);
+    } else {
+      navigation.setOptions({...TabNavOptions,tabBarStyle:{display:'none'}});
+    }
+  }, [navigation, route]);
     return (
         <Stack.Navigator
         screenOptions={{
