be49adba0c347ed4de3e38af677750fc6d800ebc
JeongBin Park <obb8923@naver.com>
2024-11-20 16:42:51 +0900

    feat/RCDSelectText

--- src/screens/RCDSelectText/index.tsx
+++ src/screens/RCDSelectText/index.tsx
@@ -10,17 +10,16 @@ import { HomeStackParamList } from "../../types/HomeStackParamList";
 const SelectButton = ({head,sub}:{head:string,sub:string}) => {
     const navigation = useNavigation<NavigationProp<HomeStackParamList>>()
     return (
-        <TouchableOpacity onPress={()=>{navigation.navigate('RCDText')}} className='w-full h-auto mb-[20]'>
+        <TouchableOpacity onPress={()=>{navigation.navigate('RCDText')}} className='w-full h-[133] mb-[20]'>
             <ShadowView>
-              <View className='flex-1 px-[33] py-[37] flex-row justify-between'>
+              <View className='pl-[33] pr-[20] py-[37] flex-row justify-between items-center'>
 
                 <View className='flex-1 justify-between'>
-                <Txt type='title4' content={head} color='yellowPrimary' />
-                <Txt type='body4' content={sub} color='gray200' />
+                <Txt type='title4' content={head} color='primary' />
+                <Txt type='body4' content={sub} color='gray_200' />
                 </View>
                 <BackIcon />
                 </View>
-
             </ShadowView>
         </TouchableOpacity>
     )
@@ -28,12 +27,12 @@ const SelectButton = ({head,sub}:{head:string,sub:string}) => {
 const RCDSelectText = () => {
     return (
         <BG type='solid'>
-            <View className='flex-1 px-px pt-[52]'>
-
+            <View className='flex-1 px-px pt-[52] items-center'>
                 <StarPNG />
-                <View className='mt-[29] gap-[19] mb-[52]'>
-                <Txt type='title2' content='집을 나서는 청년에게,\n배웅하는 말 한 마디' color='white' />
-                <Txt type='body3' content='마 우산챙기라' color='gray300' />
+                <View className='mt-[29]  mb-[52]'>
+                <Txt type='title2' content={`집을 나서는 청년에게,\n배웅하는 말 한 마디`} color='white' align='center'/>
+                <View className='mt-[19]'/>
+                <Txt type='body3' content='마 우산챙기라' color='gray_300' align='center' />
                 </View>
                 <SelectButton head='준비된 문장 읽기' sub='제시되는 문장을 수정하고 녹음하기' />
                 <SelectButton head='직접 작성하기' sub='하고싶은 말을 직접 작성하고 녹음하기' />
